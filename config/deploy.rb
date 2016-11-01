86fcccf694e6fff9c4e1b727265ccdb0e5ee0ef6
Ryota <ryt.0081@gmail.com>
2016-11-01 22:11:14 +0900

    gitのリポジトリの明記忘れ

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'facebook'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/xxxxxxxx/yyyyyyyy'
+set :repo_url, 'https://github.com/suzukisan22/facebook'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
